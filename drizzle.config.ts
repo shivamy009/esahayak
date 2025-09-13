f168657d5ec7a33bfcd65871c617faf2ab55d0d3
Shivam Yadav <shivam0342005@gmail.com>
2025-09-13 16:16:34 +0530

    refactor: update environment variable references to NEXT_PUBLIC_ prefix for consistency across the application

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default {
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.NEXT_PUBLIC_DATABASE_URL!,
   },
 } satisfies Config;
